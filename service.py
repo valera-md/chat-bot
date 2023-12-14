13c74bbff5bf4cd6f181aab91ccb269b62d05378
valera-md <150794469+valera-md@users.noreply.github.com>
2023-12-14 09:30:41 +0200

    Add files via upload

--- service.py
+++ service.py
@@ -10,33 +10,52 @@ class Bot:
 # -----
 # Hi#Hello!
 # Bye#Good bye!
-  if type(replies) == dict:
+# hm2*: check via isinstance()
+  if isinstance(replies, dict):
+  #if type(replies) == dict:
    self.replies = replies
-  elif type(replies) == str:
-   self.replies = {}
+  elif isinstance(replies, str):
+  #elif type(replies) == str:
+   repliesDict = dict()
+   #self.replies = {}
   #file = open('data/support.csv', 'r')
    file = open(replies, 'r')
+  # loop till the file ends
    while True:
     # считываем строку
     line = file.readline()
     # прерываем цикл, если строка пустая
-    if not line:
+    if line == "":
+    #if not line:
+     #print("работает")
      break
-    list = line.split("#")
-    self.replies[list[0]] = list[1]
+    fragments =  line.split("#")
+    repliesDict[fragments[0]] = fragments[1]
+    self.replies  = repliesDict
+    #list = line.split("#")
+    #self.replies[list[0]] = list[1]
    file.close()
  def replyTo(self, message):
 # hm1: try to do this using match-case
-  #match message:
-   #case "Hi":
-    #return "Hello!"
-   #case "Bye":
-    #(return "Good bye!"
+  #variant2
+  match message:
+   case "Hi":
+    return "Hello!"
+   case "Bye":
+    return "Good bye!"
+   case "What is your name?":
+    return "My name is support."
+   case _:
+    return "I can't understand you ..."
+  #variant2
   #if message == "Hi":
    #return "Hello!"
   #elif message == "Bye":
    #return "Good bye!"
-  if message in self.replies:
-   return self.replies[message]
-  else:
-   return "I can't understand you ..."
+  #elif message == "What isyour name?":
+   #return "My name is support."
+  #variant3
+  #if message in self.replies:
+   #return self.replies[message]
+  #else:
+   #return "I can't understand you ..."
