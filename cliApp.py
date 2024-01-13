c9273ce1d9a5e16cda3b162c3304d794e3ee95dd
valera-md <150794469+valera-md@users.noreply.github.com>
2024-01-13 21:22:28 +0200

    Update cliApp.py

--- cliApp.py
+++ cliApp.py
@@ -1,3 +1,4 @@
+'''
 from service import BotDict
 faqBot = BotDict("support", {
  "Hi": "Hello there", 
@@ -16,14 +17,25 @@ print(fileBot.replyTo("Hi"))
 print(fileBot.replyTo("Bye"))
 print(fileBot.replyTo("What is your name ?"))
 print(fileBot.replyTo("Do you know a joke ?"))
-
 '''
+
 #https://platform.openai.com/docs/api-reference
 #https://platform.openai.com/api-keys - создание ключа
+#https://platform.openai.com/usage
 from service import BotGPT
 gptBot = BotGPT("support", "api_key")
-print(gptBot.replyTo("Hello, are you GPT bot ?"))
+#print(gptBot.replyTo("Hello, are you GPT bot ?"))
+#print(gptBot.replyTo("What is the air temperature Moldova / Chisinau today ?")) # не натринирован работать в реальном времени, cannot provide real-time information, gpt-3.5 с 2021 года.
+print(gptBot.replyTo("Show me how to use recuests in python"))
+#hm1: make this bot CLI interactive
+while True:
+ message = input("Ask ai a question or press enter to exit: ")
+ if message != "":
+  print(gptBot.replyTo(message))
+ else:
+  exit()
 
+'''
 #pip install openai
 #via openai Python library
 from service import BotGPT2
